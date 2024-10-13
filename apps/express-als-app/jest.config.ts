290ba88cdd5903e48eb4351bbf3bf02bf5acea6f
Tayfun <dev.akguc@gmail.com>
2024-10-13 15:54:00 +0300

    create project

---
+++ apps/express-als-app/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'express-als-app',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/express-als-app',
+};
