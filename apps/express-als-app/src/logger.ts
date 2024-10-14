1437bbf3930244fb57d386296fcc73c38365ff8e
Tayfun <dev.akguc@gmail.com>
2024-10-15 00:48:46 +0300

    configure pino with pino pretty

---
+++ apps/express-als-app/src/logger.ts
@@ -0,0 +1,15 @@
+import pino from 'pino';
+
+export const logger = pino({
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+    },
+  },
+  formatters: {
+    log: (object) => {
+      return object;
+    },
+  },
+});
